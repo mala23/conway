31f30f33ffdcd254a961eb9fafe646a69f7b05b7
mala23 <mail@mala23.me>
2014-11-19 16:37:58 +0000

    write first couple of tests to apply rule one

---
+++ spec/conway_spec.rb
@@ -0,0 +1,36 @@
+# Any live cell with less than two live neighbours dies, as if caused by under-population.
+
+describe "Rule one" do
+  it '1 is less than 2' do
+    expect(1 < 2).to be true
+  end
+  it '0 is less then 2' do
+    expect(0 < 2).to be true
+  end
+  it '2 is not less then 2' do
+    expect(2 < 2).not_to be true
+  end
+
+  it 'one will die' do
+    death = true
+    expect(1 < 2).to eq death
+  end
+  it 'one will die because it has less than 2 live neighbours' do
+    live_neighbours = 2
+    death = true
+    expect(1 < live_neighbours).to eq death
+  end
+  it 'one cell will die because it has less than 2 live neighbours' do
+    live_neighbours = 2
+    death = true
+    cell = 1
+    expect(cell < live_neighbours).to eq death
+  end
+  it 'when we apply rule one on one cell with less than two neighbours it will die' do
+    live_neighbours = 2
+    death = true
+    cell = 1
+    apply_rule_one = cell < live_neighbours
+    expect(apply_rule_one).to eq death
+  end
+end
